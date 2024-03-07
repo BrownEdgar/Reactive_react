37eec8ee929e41f9cc3d66b8b8b6399ba0069b80
Raf Avetyan <rafikavetyan210907@gmail.com>
2024-03-07 20:17:53 +0400

    React-HW_3-finish2

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './React-HW_3/App'
-import './React-HW_3/index.css'
+import App from './HWs/React-HW_3/App'
+import './HWs/React-HW_3/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
