7f251476632ccdcd26ff405408af16c933c28968
Vahag123 <vahagn.nazaryan1.y@gmail.com>
2024-03-08 15:00:35 +0400

    add HW 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
+import Blog from './components/Blog/Blog'
 
 
 
 function App() {
-
   return (
     <div className='App'>
       <h1>Hello react with <span>VITE</span></h1>
+      <Blog />
     </div>
   )
 }
