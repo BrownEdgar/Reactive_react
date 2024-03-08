da8e5b5da96d9e48ee3f90c8e3b1be6ee4d66fdb
Vahag123 <vahagn.nazaryan1.y@gmail.com>
2024-03-08 13:18:00 +0400

    cleare count

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,6 @@ import './App.css'
 
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const handleClick = () => {
-    setCount(count + 1)
-  }
 
   return (
     <div className='App'>
