bab349a03b65e48e1f033659e7beeeb1096d7dce
Ero <tamaryanero@gmail.com>
2024-03-09 23:50:21 +0400

    Ero

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react'
 
-
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  const handleClick = () => {
-    setCount(count + 1)
-  }
-
+export default function App() {
   return (
-    <div className='App'>
-      <h1>Hello react with <span>VITE</span></h1>
-    </div>
+    <div>App</div>
   )
 }
-
-export default App
---
+++ src/homeworks/homework1/App.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import './App.scss'
+import { useState } from 'react'
+
+export default function App() {
+    const [data, setdata] = useState(
+        [
+            {
+             id: 1,
+             title: "Terminator",
+             genre: "Action",
+             stock: 6,
+             rate: 2.5,
+            },
+            {
+             id: 2,
+             title: "Die Hard",
+             genre: "Action",
+             stock: 5,
+             rate: 2.5,
+            },
+            {
+             id: 3,
+             title: "Get Out",
+             genre: "Thriller",
+             stock: 8,
+             rate: 3.5,
+            },
+            {
+             id: 4,
+             title: "Trip to Italy",
+             genre: "Comedy",
+             stock: 7,
+             rate: 3.5,
+            },
+            {
+             id: 5,
+             title: "Airplane",
+             genre: "Comedy",
+             stock: 7,
+             rate: 3.5,
+            },
+        ]
+    )
+  return(
+    <div className='App'>
+        <h1 className='App__title'>Showing 9 movies in the database</h1>
+        <div className='App__content'>
+            
+        </div>
+    </div>
+  )
+}
