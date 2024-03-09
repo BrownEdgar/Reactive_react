bea9aac42030381a7c54a015040c91ceb232c027
Raf Avetyan <rafikavetyan210907@gmail.com>
2024-03-09 12:52:46 +0400

    React-HW_4-finish

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './HWs/React-HW_3/App'
-import './HWs/React-HW_3/index.css'
+import App from './HWs/React-HW_4/App'
+import './HWs/React-HW_4/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
