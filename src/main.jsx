54e39a52884705a6a0f92786d541cab9fa485d59
BrownEdgar <nikoghosjanedgar@gmail.com>
2024-03-11 18:27:19 +0400

    useEffect

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './examples/1/App'
+import App from './lessons/hooks/useEffect/App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
