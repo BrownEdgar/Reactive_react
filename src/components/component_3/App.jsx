2e111517bd04b1a214d000336d915427a8582469
Ero <tamaryanero@gmail.com>
2024-03-13 00:23:18 +0400

    ero

--- src/components/component_3/App.jsx
+++ src/components/component_3/App.jsx
@@ -2,6 +2,22 @@ import React, {useState, useEffect} from 'react'
 import './App.scss'
 
 export default function Users() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const getData = () => {
+      fetch('https://jsonplaceholder.typicode.com/Users?_limit=9')
+        .then((res) => res.json())
+        .then(data => setData(data))
+    }
+    getData()
+  }, [])
+
+  const handleDelete = (id) => {
+    const result = data.filter(elem => elem.id !== id)
+    setData(result)
+  }
+
   return (
     <div>App</div>
   )
--- src/components/component_4/App.jsx
+++ src/components/component_4/App.jsx
@@ -2,7 +2,23 @@ import React, {useState, useEffect} from 'react'
 import './App.scss'
 
 export default function Posts() {
-  return (
-    <div>App</div>
-  )
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+      const getData = () => {
+        fetch('https://jsonplaceholder.typicode.com/posts?_limit=9')
+          .then((res) => res.json())
+          .then(data => setData(data))
+      }
+      getData()
+    }, [])
+  
+    const handleDelete = (id) => {
+      const result = data.filter(elem => elem.id !== id)
+      setData(result)
+    }
+  
+    return (
+      <div>App</div>
+    )
 }
