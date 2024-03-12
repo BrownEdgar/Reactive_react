28bd648442d5f49bdccd591b4d549ccbc6339932
Ghazaryan07 <vaheghazaryann07@gmail.com>
2024-03-12 22:52:18 +0400

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-import HomeWork4 from "./HomeWorks/HomeWork4/HW4"
-import HomeWork5 from "./HomeWorks/HomeWork5/HW5"
+import HomeWork6 from "./HomeWorks/HomeWork6/App"
 
 
 function App() {
 
     return(
-      <HomeWork5/>
+      <HomeWork6/>
     )
 }
 
