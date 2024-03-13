f0de0ae68d4376377fb43da7ea0168d32711b024
unknown <Davit>
2024-03-13 16:36:04 +0400

    line

---
+++ src/Lessons/hooks/useEffect/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import "./App.scss";
+
+function App() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        console.log("use effect run")
+        const getData = () => {
+            fetch("https://jsonplaceholder.typicode.com/posts?_limit=9")
+                .then((response) => response.json())
+                .then((json) => setData(json));
+        };
+        getData();
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>Our post</h1>
+            <div className="App__posts">
+                {
+                    data.map((post) => {
+                    return (
+                        <div key={post.id}>
+                            <h2>{post.title}</h2>
+                            <p>{post.body}</p>
+                            <button>
+                                <i class="bx bx-trash"></i>
+                            </button>
+                        </div>
+                    );
+                    })
+                }
+            </div>
+        </div>
+    );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./examples/1/App";
+import App from "./Lessons/hooks/useEffect/App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
