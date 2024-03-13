cf969d8b1f44cb4cb013347643858f6e8589298a
BrownEdgar <nikoghosjanedgar@gmail.com>
2024-03-13 18:21:25 +0400

    axios & content loader

--- src/Homeworks/HW4/hooks/useEffect/App.jsx
+++ src/Homeworks/HW4/hooks/useEffect/App.jsx
@@ -1,67 +1,70 @@
 import React, { useState, useEffect } from 'react'
+import instance from './axios'
+import classNames from 'classnames'
+import axios from 'axios'
+
 import './App.scss'
+import Posts from '../../../../components/Posts/Posts'
+import PostLoader from '../../../../components/Loaders/PostsLoader'
+
+const arr = ['posts', 'todos', 'comments', 'users',]
 
 export default function App() {
   const [data, setData] = useState([])
-  const getToDos = () => {
-    fetch('https://jsonplaceholder.typicode.com/todos?_limit=9')
-      .then((res) => res.json())
-      .then(data => setData(data));
-    // getUsers([])
-    // getComents([])
-    // getPosts([])
-  }
-  const getComments = () => {
-    fetch('https://jsonplaceholder.typicode.com/comments?_limit=9')
-      .then((res) => res.json())
-      .then(data => setData(data));
-    // getUsers([])
-    // getPosts([])
-    // getToDos([])
-  }
-  const getUsers = () => {
-    fetch('https://jsonplaceholder.typicode.com/users?_limit=9')
-    .then((res) => res.json())
-    .then(data => setData(data));
-    // getPosts([])
-    // getComents([])
-    // getToDos([])
-  }
-  const getPosts = () => {
-    fetch('https://jsonplaceholder.typicode.com/posts?_limit=9')
-      .then((res) => res.json())
-      .then(data => setData(data));
-      // getUsers([])
-      // getComents([])
-      // getToDos([])
+  const [option, setOption] = useState('posts')
+  const [loading, setLoading] = useState(true)
+
+
+  useEffect(() => {
+    const getData = () => {
+      // axios(`https://jsonplaceholder.typicode.com/${option}`, {
+      //   params: {
+      //     _limit: 9,
+      //     _start: 49
+      //   }
+      // })
+      // axios({
+      //   baseURL: 'https://jsonplaceholder.typicode.com/',
+      //   url: option,
+      //   params: {
+      //     _limit: 12
+      //   }
+      // })
+      setLoading(true)
+      instance(option, {
+        params: {
+          _limit: 9
+        }
+      })
+        .then((res) => setData(res.data))
+        .finally(() => setLoading(false))
     }
-    useEffect(() => {
-      getToDos()
-      getComments()
-      getUsers()
-      getPosts()
-    }, [])
-    
+    // const data = {
+    //   userId: 1,
+    //   title: 'Lorem ipsum dolor sit.',
+    //   body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum molestiae totam ad obcaecati quidem?'
+    // }
+    // axios.post('https://jsonplaceholder.typicode.com/posts', data)
+    //   .then((res) => console.log(res));
+
+    getData()
+  }, [option])
+
   return (
     <div className='App'>
       <div className='App__btn'>
-        <button onClick={getPosts}>Get Posts</button>
-        <button onClick={getUsers}>Get Users</button>
-        <button onClick={getComments}>Get Comments</button>
-        <button onClick={getToDos}>Get ToDos</button>
+        {arr.map(elem => {
+          return <button
+            className={classNames('btn', {
+              active: elem === option,
+            })}
+            onClick={() => setOption(elem)}
+          >Get {elem}</button>
+        })}
       </div>
-      <div className='App__data'>
-        {
-          data.map(elem => {
-            return (
-              <div key={elem.id}>
-                <h2>{elem.title || elem.name || elem.email}</h2>
-                <p>{elem.body || elem.username}</p>
-              </div>
-            )
-          })
-        }
-      </div>
+
+      <Posts data={data} loading={loading} />
+
     </div>
   )
 }
