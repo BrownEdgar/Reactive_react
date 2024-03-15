bf385a38e6dbb15686ce1d282dbcbfdb10b85317
BrownEdgar <nikoghosjanedgar@gmail.com>
2024-03-15 18:12:12 +0400

    propTypes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './lessons/hooks/useEffect/App'
+import App from './lessons/Todos/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
