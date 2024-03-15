c850d9ed7215ea21e901939cf765461947031e4a
Ero <tamaryanero@gmail.com>
2024-03-15 08:52:50 +0400

    ero

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
-import Todos from './components/component_1/App'
-import Comments from './components/component_2/App'
-import Users from './components/component_3/App'
-import Posts from './components/component_4/App'
+import Todos from './Homeworks/homework/App'
+
 
 export default function App() {
   return (
     <div>
       <Todos/>
-      <Comments/>
-      <Users/>
-      <Posts/>
     </div>
   )
 }
---
+++ src/homeworks/homework/App.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect} from 'react'
+import axios from 'axios'
+import './App.scss'
+
+export default function Todos() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const getData = () => {
+        setLoading(true)
+      axios('https://jsonplaceholder.typicode.com/Todos?_limit=9')
+        .then((res) => setData(res.data))
+        .finally(() => setLoading(false))
+    }
+    getData()
+  }, [data])
+
+  const handleDelete = (id) => {
+    const result = data.filter(elem => elem.id !== id)
+    setData(result)
+  }
+
+  return (
+    <div className='Todos'>
+      <h1 className='Todos__title'>Our Todos</h1>
+      <div className="Todos__body">
+        {
+          data.map(todos => {
+            return (
+              <div key={todos.id}>
+                <h2>{todos.title}</h2>
+                <button onClick={() => handleDelete(todos.id)}>
+                  <i className='bx bxs-message-square-x bx-md'></i>
+                </button>
+              </div>
+            )
+          })
+        }
+      </div>
+      {
+        loading ? <h1>loading...</h1> : <Todos data={data}/>
+      }
+    </div>
+  )
+}
