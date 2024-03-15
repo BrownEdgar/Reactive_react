2e145eab57e0e93873fd325a9a4e2c24d411504d
unknown <Davit>
2024-03-15 16:37:55 +0400

    last

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,10 @@
 // import App from "./Lessons/hooks/useEffect/App";
 // import "./index.css";
 // =======
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './Apply.jsx'
-import './index.css'
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./Apply.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
