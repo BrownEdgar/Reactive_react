3be1c96b2c1df458c85d11f01e785bc1502596fe
Ero <tamaryanero@gmail.com>
2024-03-20 00:55:36 +0400

    ero

--- src/homeworks/homework_2/App.jsx
+++ src/homeworks/homework_2/App.jsx
@@ -1,29 +1,7 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import './App.scss'
-import Comments from './Comments/App'
+import React from 'react'
 
-const DB_URL = import.meta.env.VITE_DB_URL
-
-export default function User() {
-  const [data, setData] = useState([])
-
-  useEffect(() => {
-    const getData = () => {
-      axios(DB_URL + 'comments?_limit=9')
-        .then((res) => setData(res.data))
-    }
-
-    getData()
-  }, [])
-
+export default function App() {
   return (
-    <div>
-      {
-        data.map(elem => {
-          return <Comments elem={elem} key={elem.id} />
-        })
-      }
-    </div>
+    <div>App</div>
   )
 }
--- src/homeworks/homework_2/Comments/App.jsx
+++ src/homeworks/homework_2/Comments/App.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import './App.scss'
 
-export default function Comments({ elem }) {
+const DB_URL = import.meta.env.VITE_DB_URL
+
+export default function User() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const getData = () => {
+      axios(
+        {
+          baseURL: DB_URL,
+          url: 'comments',
+          params:{
+            _limit:9
+          }
+        }
+      ).then((res) => setData(res.data))
+    }
+    getData()
+  }, [])
+
   return (
-    <div>
-      
+    <div className='User'>
+      <h1>Comments</h1>
+      <div className='User__comments'>
+        {
+          data.map(elem => {
+            return(
+              <div key={elem.id}>
+                <h3>{elem.body}</h3>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
