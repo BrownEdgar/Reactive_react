9bcea985976a2046137eed89d4aa909b5b76eaf9
Ero <tamaryanero@gmail.com>
2024-03-20 00:25:10 +0400

    ero

--- src/homeworks/homework_2/App.jsx
+++ src/homeworks/homework_2/App.jsx
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.scss'
-import Quotes from './Quotes/App'
+import Comments from './Comments/App'
+
+const DB_URL = import.meta.env.VITE_DB_URL
 
 export default function User() {
   const [data, setData] = useState([])
 
   useEffect(() => {
     const getData = () => {
-      axios('https://dummyjson.com/quotes')
-        .then((res) => setData(res.data.quotes))
+      axios(DB_URL + 'comments?_limit=9')
+        .then((res) => setData(res.data))
     }
 
     getData()
@@ -19,7 +21,7 @@ export default function User() {
     <div>
       {
         data.map(elem => {
-          return <Quotes elem={elem} key={elem.id} />
+          return <Comments elem={elem} key={elem.id} />
         })
       }
     </div>
