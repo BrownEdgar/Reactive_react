8a9976e223006af01c83d6949e76d2b37e82af96
Ero <tamaryanero@gmail.com>
2024-03-21 20:59:07 +0400

    ero

---
+++ src/Modal/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.scss'
+
+export default function Modal({closeModal}) {
+  return (
+    <div className='Modal'>
+      <div className="Modal__content">
+        <h1>You are welcome!</h1>
+        <button className='Modal__btn' onClick={closeModal}>
+          Close
+        </button>
+      </div>
+    </div>
+  )
+}
+
--- src/homeworks/homework_2/App.jsx
+++ src/homeworks/homework_2/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import Modal from './Modal/App'
+import Modal from '../../Modal/App'
 import './App.scss'
 
 const DB_URL = import.meta.env.VITE_DB_URL
