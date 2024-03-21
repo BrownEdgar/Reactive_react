b58eaa625100f9fcd05cbca604a374a0f2f426ed
Ero <tamaryanero@gmail.com>
2024-03-21 21:52:50 +0400

    ero

--- src/homeworks/homework_3/Form.jsx
+++ src/homeworks/homework_3/Form.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './Form.scss'
+import Modal from '../../Modal/Modal'
 
 export default function Form() {
+  const [users, setUsers] = useState([
+    {
+      id:1,
+      username:'Ero',
+      email:'tamaryanero@gmail.com'
+    },
+    {
+      id:2,
+      username:'Mher',
+      email:'navasardyanmher@gmail.com'
+    },
+    {
+      id:3,
+      username:'Tiko',
+      email:'tikchxrikyan@gmail.com'
+    },
+    {
+      id:3,
+      username:'Davo',
+      email:'davidhakhinyan@gmail.com'
+    }
+  ])
+
+  const handleSubmit = (e) => {
+    e.target.preventDefault()
+  }
+
   return (
     <div className='Form'>
-        <form>
-          <input type="text" />
+        <form onSubmit={handleSubmit}>
+          <input type="text" name='username' placeholder='Username'/>
+          <input type="password" name='password' placeholder='password'/>
+          <input type="email" name='email' placeholder='Your Email'/>
+          <input type="Submit" />
         </form>
     </div>
   )
