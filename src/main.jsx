e78dd1c892e33f1e3eace6535441d86aded7b994
unknown <Davit>
2024-03-22 16:33:48 +0400

    HW10

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./examples/2(modal)/App";
+import App from "./Lessons/Lesson9/2(modal)/App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
