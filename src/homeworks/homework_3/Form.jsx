32e4538f8345e8f5102ecda14df8fa6a3f68542d
Ero <tamaryanero@gmail.com>
2024-03-23 18:58:18 +0400

    yet

--- src/homeworks/homework_3/Form.jsx
+++ src/homeworks/homework_3/Form.jsx
@@ -8,25 +8,21 @@ export default function Form() {
       id:1,
       username:'Ero',
       email:'tamaryanero@gmail.com',
-      password: 'Ero'
     },
     {
       id:2,
       username:'Mher',
       email:'navasardyanmher@gmail.com',
-      password: 'Mher'
     },
     {
       id:3,
       username:'Tiko',
       email:'tikchxrikyan@gmail.com',
-      password: 'Tiko'
     },
     {
       id:4,
       username:'Davo',
       email:'davidhakhinyan@gmail.com',
-      password: 'Davo'
     }
   ])
   const [isopen, setIsopen] = useState(false)
@@ -35,9 +31,7 @@ export default function Form() {
     e.preventDefault();
     const { email, username, password } = e.target
     const isUserExist = users.some(elem => {
-      elem.email === email.value.toLowerCase() && 
-      elem.username === username.value.toLowerCase() &&
-      elem.password === password.value.toLowerCase()
+      elem.email === email.value.toLowerCase() 
     })
     if (isUserExist) {
       toggleModal();
@@ -52,7 +46,7 @@ export default function Form() {
   return (
     <div className='Form'>
       {
-        !isopen ?
+        isopen ?
         <Modal>
           <h1>You are logined successfully</h1>
           <button className='Modal__btn' onClick={toggleModal}>Ok</button>
@@ -60,12 +54,18 @@ export default function Form() {
       }
       <form onSubmit={handleSubmit}>
         <h1>Login form</h1>
-        <i class='bx bxl-gmail'></i>
-        <input type="text" name='username' placeholder='Username'/>
-        <i class='bx bxs-lock'></i>
-        <input type="password" name='password' placeholder='password'/>
-        <i class='bx bxs-user-circle' ></i>
-        <input type="email" name='email' placeholder='Your Email'/>
+        <div>
+          <i className='bx bxs-user-circle' ></i>
+          <input type="text" name='username' placeholder='Username'/>
+        </div>
+        <div>
+          <i className='bx bxs-lock'></i>
+          <input type="password" name='password' placeholder='password'/>
+        </div>
+        <div>
+          <i className='bx bxl-gmail'></i>
+          <input type="email" name='email' placeholder='Your Email'/>
+        </div>
         <input type="Submit" value='Login'/>
       </form>
     </div>
