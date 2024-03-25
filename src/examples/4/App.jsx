d4a6d87b005a140ef2cd3fecfb474fab2c89369f
unknown <Davit>
2024-03-25 18:21:32 +0400

    Lesson 12

--- src/examples/4/App.jsx
+++ src/examples/4/App.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { nanoid } from "nanoid";
 import "./App.scss";
 
 function App() {
     const [users, setUsers] = useState([]);
     const [showPassword, setShowPassword] = useState(false);
+    const [currentUserId, setCurrentUserId] = useState(null);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -13,13 +14,31 @@ function App() {
             id: nanoid(5),
             username: username.value,
             password: password.value,
-        };
-        setUsers([...users, user]);
+        }
+        setUsers((prevUsers) => {
+            const newUsers = [...prevUsers, user]
+            localStorage.setItem('users', JSON.stringify(newUsers))
+            return newUsers;
+        })
     };
+
+    useEffect(() => {
+        const users = JSON.parse(localStorage.getItem('users')) || [];
+        setUsers(users)
+    }, [])
 
     const togglePassword = () => {
         setShowPassword(!showPassword);
     };
+
+    const deleteById = (id) => {
+        const result = users.filter(elem => elem.id !== id)
+        setUsers(result)
+    };
+    
+    const changeCurrentUserId = (userId) => {
+        setCurrentUserId((userId === currentUserId) ? null : userId)
+    }
 
     return (
         <div className="App">
@@ -64,7 +83,18 @@ function App() {
                             <tr key={elem.id}>
                                 <td>{elem.id}</td>
                                 <td>{elem.username}</td>
-                                <td>{elem.password}</td>
+                                <td className="password">
+                                    {currentUserId == elem.id
+                                        ? elem.password
+                                        : "*".repeat(10)}
+                                    <i className={`bi ${(currentUserId == elem.id)? "bi-eye-slash": "bi bi-eye"}`} onClick={() => changeCurrentUserId(elem.id)}></i>
+                                </td>
+                                <td className="delete">
+                                    <i
+                                        className="bi bi-x-octagon"
+                                        onClick={() => deleteById(elem.id)}
+                                    ></i>
+                                </td>
                             </tr>
                         );
                     })}
