2edeae25eda3dbfbdfc0f146864accd44f0ca0bc
Ero <tamaryanero@gmail.com>
2024-03-31 21:37:02 +0400

    sdgbfb

--- src/homeworks/homework_6/App.jsx
+++ src/homeworks/homework_6/App.jsx
@@ -1,9 +1,13 @@
 import React, {useState, useEffect} from 'react'
+import {ErrorMessage, Field, Formik, Form } from 'formik'
+import { string, object } from 'yup'
 import './App.scss'
 
 
 export default function App() {
   return (
-    <div>App</div>
+    <div className='App'>
+      <Formik></Formik>
+    </div>
   )
 }
