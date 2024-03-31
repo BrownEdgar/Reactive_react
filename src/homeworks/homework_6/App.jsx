5f0651bb38772c27d73bcd78932117bfcd27b77b
Ero <tamaryanero@gmail.com>
2024-03-31 22:10:12 +0400

    ero

--- src/homeworks/homework_6/App.jsx
+++ src/homeworks/homework_6/App.jsx
@@ -1,10 +1,20 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import {ErrorMessage, Field, Formik, Form } from 'formik'
 import { string, object } from 'yup'
 import './App.scss'
 
 
+const validationSchema = object({
+  email: string().email('must be emil definetly').required('important!'),
+  username: string().matches(/^[A-Z]/, "must start with uppercase").min(3, "too short").max(10, "too long").required("important!"),
+  password: string().min(8, "too short").required("important!")
+})
+
 export default function App() {
+  const [users, setUsers] = useState([])
+  const [isOpen, setIsOpen] = useState(false)
+  const [currentIndex, setCurrentIndex] = useState(second)
+  
   return (
     <div className='App'>
       <Formik
@@ -21,8 +31,19 @@ export default function App() {
         {(formik) => (
           <Form>
             <div className="form__email">
+              <label htmlFor="email">Email</label>
               <Field type="email" name="email" placeholder="Email" />
               <ErrorMessage name="email" className="error" component="p" />
+            </div>
+            <div className="form__userName">
+              <label htmlFor="username">Username</label>
+              <Field type="text" name="username" placeholder="Username" />
+              <ErrorMessage name="username" className="error" component="p" />
+            </div>
+            <div className="form__password">
+              <label htmlFor="password">Password</label>
+              <Field type="password" name="password" placeholder="Password" />
+              <ErrorMessage name="password" className="error" component="p" />
             </div>
           </Form>
         )}
