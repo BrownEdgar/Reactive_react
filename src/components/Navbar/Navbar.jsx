2fcd24cb808e9b83adb915e0411ecf1c3080daac
Ero <tamaryanero@gmail.com>
2024-04-05 00:12:09 +0400

    tdmthm

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import './Navbar.scss'
 
 export default function Navbar() {
   return (
-    <div className='Navbar'>
-      <header>
-        
-      </header>
-    </div>
+    <header>
+      <div className="bakery">
+        <Link to="/">Bakery</Link>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Our products</Link>
+          </li>
+          <li>
+            <Link to="/news">News</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact us</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
