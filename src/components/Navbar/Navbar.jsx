78c231073dcf82db401b68205a1879f8532f899f
Ero <tamaryanero@gmail.com>
2024-04-04 23:56:06 +0400

    eri

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Navbar.scss'
+
+export default function NavBar() {
+  return (
+    <div>NavBar</div>
+  )
+}
---
+++ src/pages/products/Products.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Products.scss'
+
+export default function Products() {
+  return (
+    <div className='Products'>
+        <h1>Products page</h1>
+    </div>
+  )
+}
