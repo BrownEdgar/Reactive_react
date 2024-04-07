74a06d566acd093b5e326b19081cd5fbb75f7531
Ero <tamaryanero@gmail.com>
2024-04-07 16:41:46 +0400

    yeum

---
+++ src/pages/news/News.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './News.scss'
+
+export default function News() {
+  return (
+    <div className='Contact'>
+        <h1>News page</h1>
+    </div>
+  )
+}
---
+++ src/pages/products/Products.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Contact.scss'
+
+export default function Contact() {
+  return (
+    <div className='Contact'>
+        <h1>Our products</h1>
+    </div>
+  )
+}
