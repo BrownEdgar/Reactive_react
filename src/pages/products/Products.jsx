0a467f59375f71864e3f656d21d89ec1f0d47ded
Ero006 <tamaryanero@gmail.com>
2024-04-10 00:32:30 +0400

    ero

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,18 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import './Contact.scss'
+import { Link } from 'react-router-dom'
 
 export default function Products() {
   const [data, setData] = useState([])
 
   useEffect(() => {
     axios('https://fakestoreapi.com/products')
-    .then(res => setData(res))
+    .then(res => setData(res.data))
   })
   
   return (
     <div className='Products'>
-        <h1>Our products</h1>
+      {data.map((elem) => {
+        return 
+          <Link key={elem.id}>
+            <h1>{elem.title}</h1>
+            <img src={elem.image} alt="" />
+          </Link>
+      })}
     </div>
   )
 }
