e6b7dff4a7f4eb9a70718932e15157f84fde9f10
BrownEdgar <nikoghosjanedgar@gmail.com>
2024-04-10 17:20:27 +0400

    debounse effect

--- src/pages/Posts/Posts.jsx
+++ src/pages/Posts/Posts.jsx
@@ -3,36 +3,44 @@ import { Formik, Form, Field } from 'formik'
 import { Link } from 'react-router-dom';
 import './Posts.scss'
 
-const searchPosts = (posts, value) => {
-  if (value === '') return posts;
-  return posts.filter(elem => elem.title.includes(value.toLowerCase()))
-}
+
 
 
 export default function Posts() {
   const [posts, setPosts] = useState([]);
+  const [filteredPost, setfilteredPost] = useState([]);
   const [searchValue, setSearchValue] = useState('');
   const [timeOutId, setTimeOutId] = useState(null)
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(res => res.json())
-      .then(data => setPosts(data))
+      .then(data => {
+        setPosts(data)
+        setfilteredPost(data)
+      })
   }, [])
 
-
-
+  useEffect(() => {
+    const searchPosts = (posts, value) => {
+      console.log('searchPosts')
+      if (value === '') return posts;
+      return posts.filter(elem => elem.title.includes(value.toLowerCase()))
+    }
+    setfilteredPost(() => {
+      return searchPosts(posts, searchValue)
+    })
+  }, [searchValue])
   const handleChange = (e) => {
     if (timeOutId !== null) {
       clearTimeout(timeOutId)
     }
     const timer = setTimeout(() => {
-      console.log(1111)
       setSearchValue(() => e.target.value)
     }, 1000)
     setTimeOutId(timer)
-
   }
+
   return (
     <div className='Posts'>
       <h1>{searchValue}</h1>
@@ -40,7 +48,7 @@ export default function Posts() {
         <input type="search" placeholder='search' name="search" onChange={handleChange} />
       </form>
 
-      {searchPosts(posts, searchValue).map(elem => {
+      {filteredPost.map(elem => {
         return (
           <Link key={elem.id} to={`${elem.id}`}>
             <h2>{elem.title}</h2>
