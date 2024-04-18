4ded6b33e7eb389c047e3ad2e2931df9be389951
ArsenPalyan <arsenpalyan5@gmail.com>
2024-04-18 22:37:01 +0400

    react14

--- src/React 14/useArray.jsx
+++ src/React 14/useArray.jsx
@@ -1,12 +1,34 @@
 import React, { useState } from 'react'
 
 export default function useArray() {
-  const [data, setData] = useState(initialState)
 
-  const push = (elem) =>{
-    setData((state) => [...state, elem]) 
-  }
+  const useArray  = (initialState) => {
+    const [data, setData] = useState(initialState);
 
+    const filter = (elem) => {
+      setData((prevState) => prevState.filter(elem));
+    };
+
+    
+    const remove = (index) => {
+      setData((initState) => initState.filter((_, i) => i !== index));
+    };
+    
+    const push = (elem) => {
+    setData((initState) => [...initState, elem]);
+    };
+
+    const set = () => {
+      setData([1, 2])
+    }
+
+    const clear = () => {
+      setData([]);
+    };
+
+
+    return { data, filter , push, remove, clear, set };
+  };
 
   return (
     <div>useArray</div>
