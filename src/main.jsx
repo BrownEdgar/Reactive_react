51ca6248643d045d85ae644f87c110ced15296e3
BrownEdgar <nikoghosjanedgar@gmail.com>
2024-04-19 16:41:40 +0400

    custom hook

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './examples/7(slider)/App'
+import App from './examples/8(customHook)/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
