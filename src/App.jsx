d4b9a7a29dc19f0fc6b8350d765872564340ab0f
Ero006 <tamaryanero@gmail.com>
2024-04-25 22:17:21 +0400

    ero

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
 import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import {Routes, Route} from 'react-router-dom'
-import ROUTES from './routes'
-import {Home, News, Products, Contact, Product} from './pages/index'
 
 export default function App() {
   return (
     <div className='App'>
-      <Navbar/>
-      <Routes>
-          <Route path={ROUTES.HOME} element={<Home/>}/>
-          <Route path={ROUTES.NEWS} element={<News/>}/>
-          <Route path={ROUTES.PRODUCTS} element={<Products/>}/>
-          <Route path={ROUTES.PRODUCT} element={<Product/>}/>
-          <Route path={ROUTES.CONTACT} element={<Contact/>}/>
-      </Routes>
+
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './homeworks/homework_11/App'
-import {BrowserRouter as Router} from 'react-router-dom'
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router>
+  <React.StrictMode>
     <App />
-  </Router>,
+  </React.StrictMode>,
 )
