3637080a270489a871b8feedb37aefa84ada39b9
Ero006 <tamaryanero@gmail.com>
2024-04-26 00:25:16 +0400

    ;se

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,21 @@ export default function App() {
   })
   return (
     <div className='App'>
-      {todos.map(elem => {
-        
-      })}
+      <h1 className='Todos__title'>Our Todos</h1>
+      <div className="Todos__body">
+        {
+          todos.map(todos => {
+            return (
+              <div key={todos.id}>
+                <h2>{todos.title}</h2>
+                <button onClick={() => value.handleDelete(todos.id)}>
+                  <i className="bi bi-x-circle-fill"></i>
+                </button>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
