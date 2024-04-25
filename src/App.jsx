db58bbb8f2fa5e49af6f7e61ab4991db961fde92
Vahag123 <vahagn.nazaryan1.y@gmail.com>
2024-04-25 16:57:51 +0400

    Lesson 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react'
 
-
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  const handleClick = () => {
-    setCount(count + 1)
-  }
-
+export default function App() {
   return (
-    <div className='App'>
-      <h1>Hello react with <span>VITE</span></h1>
-      <button onClick={handleClick}>count: {count} </button>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere doloribus nisi ipsum omnis modi magni quaerat necessitatibus excepturi nemo id sequi nesciunt, architecto suscipit minima labore quo distinctio aspernatur fugiat molestias optio reprehenderit perspiciatis? Deserunt nam reprehenderit tenetur veniam veritatis?</p>
+    <div>
     </div>
   )
 }
-
-export default App
---
+++ src/Lesson1/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.scss'
+
+
+export default function App() {
+  return (
+    <div className='App'>
+      <h1 className='wrapper'>Hello World</h1>
+    </div>
+  )
+}
